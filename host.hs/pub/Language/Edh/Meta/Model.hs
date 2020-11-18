8028a6043a1b41d7d3505ca2db4ad2674ef6eb41
ComplYue <compl.yue@icloud.com>
2020-11-18 22:48:10 +0800

    wip

--- host.hs/pub/Language/Edh/Meta/Model.hs
+++ host.hs/pub/Language/Edh/Meta/Model.hs
@@ -1,6 +1,31 @@
-
 module Language.Edh.Meta.Model where
 
+-- import           Debug.Trace
+
+import           Control.Concurrent.STM
+import           Control.Monad
+import           Control.Monad.ST
+import           Data.HashMap.Strict            ( HashMap )
+import qualified Data.HashMap.Strict           as Map
+import           Data.Vector                    ( Vector )
+import qualified Data.Vector                   as V
+import qualified Data.Vector.Mutable           as MV
+import           Language.Edh.EHI
 import           Prelude
 
+data EL'Scope = EL'Scope
+  { el'scope'src'loc :: !SrcLoc,
+    -- a new region is created upon each attr definition or deletion
+    el'scope'regions :: !EL'Region
+  }
 
+data EL'Region = EL'Region
+  { el'region'src'span :: !SrcRange,
+    el'region'attrs :: !(HashMap AttrKey EL'Arti)
+  }
+
+data EL'Arti = EL'Arti
+  { el'arti'origin :: !SrcLoc,
+    el'arti'type :: !(Maybe EdhTypeValue),
+    el'arti'value :: !(Maybe EdhValue)
+  }
