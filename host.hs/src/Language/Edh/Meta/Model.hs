57d9af761d18625167cfbfeb3726179231e22488
ComplYue <compl.yue@icloud.com>
2020-11-20 22:55:24 +0800

    wip

--- host.hs/src/Language/Edh/Meta/Model.hs
+++ host.hs/src/Language/Edh/Meta/Model.hs
@@ -3,19 +3,102 @@ module Language.Edh.Meta.Model where
 -- import           Debug.Trace
 
 import Control.Concurrent.STM
-import Control.Monad
-import Control.Monad.ST
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as Map
 import Data.Hashable
 import Data.Text (Text)
-import qualified Data.Text as T
 import Data.Vector (Vector)
 import qualified Data.Vector as V
-import qualified Data.Vector.Mutable as MV
 import Language.Edh.EHI
 import Language.Edh.RtTypes
 import Prelude
+
+data EL'AttrKey = EL'AttrKey
+  { el'akey'src :: !AttrAddrSrc,
+    el'akey'reified :: !(Maybe AttrKey)
+  }
+  deriving (Show)
+
+instance Eq EL'AttrKey where
+  EL'AttrKey _ (Just k1) == EL'AttrKey _ (Just k2) = k1 == k2
+  EL'AttrKey (AttrAddrSrc addr1 _) Nothing
+    == EL'AttrKey (AttrAddrSrc addr2 _) Nothing = addr1 == addr2
+  EL'AttrKey {} == EL'AttrKey {} = False -- or hash/eq laws won't hold
+
+instance Hashable EL'AttrKey where
+  hashWithSalt s (EL'AttrKey _ (Just key)) =
+    s `hashWithSalt` (1 :: Int) `hashWithSalt` key
+  hashWithSalt s (EL'AttrKey (AttrAddrSrc addr _) Nothing) =
+    s `hashWithSalt` (2 :: Int) `hashWithSalt` addr
+
+data EL'Value
+  = -- | represent values other than a class or an object
+    EL'Value
+      { el'value'src :: !ExprSrc,
+        -- TODO this usefull ??
+        el'value'type :: !(Maybe EdhTypeValue),
+        -- staticly decidable value, or nil if unable to
+        el'value'refied :: !EdhValue
+      }
+  | -- | a class definition is specially treated in static analysis
+    EL'Class
+      { el'class'proc :: !ProcDecl,
+        el'class'scope :: !EL'Scope,
+        -- | an attribute is exported by any form of assignment targeting
+        -- current scope, or any form of procedure declaration, which follows an
+        -- `export` keyword, or within a block following an `export` keyword
+        el'class'exports :: !EL'Exports
+      }
+  | -- | an object instance value is specially treated in static analysis
+    EL'Object
+      { -- | the class of this object instance
+        -- TODO use some other, more proper type for this field ?
+        el'obj'class :: !EL'OriginalValue,
+        -- | the src instantiated this object
+        el'obj'inst'src :: !ExprSrc
+      }
+
+-- | Edh module and `.edh` text doc (os file, virtual or physical, local or
+-- remote) be of 1:1 mapping
+data EL'Module = EL'Module
+  { -- | each parent dir of `edh_modules` is considered an Edh home
+    el'modu'home :: !EL'Home,
+    -- | absolute path of the `.edh` src file
+    el'modu'doc :: !SrcDoc,
+    -- | there will be nested scopes appearing in natural source order, within
+    -- this root scope of the module
+    el'modu'scope :: !EL'Scope,
+    -- | an attribute is exported by any form of assignment targeting
+    -- current scope, or any form of procedure declaration, which follows an
+    -- `export` keyword, or within a block following an `export` keyword
+    el'modu'exports :: !EL'Exports
+  }
+
+data EL'Home = EL'Home
+  { -- | each parent dir of `edh_modules` is considered an Edh home
+    el'home'path :: !Text,
+    -- | loaded modules under this home
+    --
+    -- module name is path (with `.edh` and `/__init__.edh` stripped off), and
+    -- relative to the `edh_modules` dir
+    --
+    -- special case is `/__main__.edh`, from which `.edh` is stripped off, but
+    -- the `__main__` part remains in the module name
+    el'home'modules :: !(TMVar (Map.HashMap ModuleName EL'Module))
+  }
+
+type ModuleName = Text
+
+-- | the dict of artifacts by attribute key, for those exported from an object
+-- (a module object or vanilla object), with their order of export preserved
+type EL'Exports = OrderedDict AttrKey EL'OriginalValue
+
+data EL'OriginalValue = EL'OriginalValue
+  { el'origin'module :: !EL'Module,
+    el'origin'scope :: !EL'Scope,
+    el'origin'region :: !EL'Region,
+    el'value :: !EL'Value
+  }
 
 data EL'Section = EL'ScopeSec !EL'Scope | EL'RegionSec !EL'Region
 
@@ -28,7 +111,7 @@ data EL'Scope = EL'Scope
     -- a scope can have nested scopes, and it at least should have a final
     -- region to contain all possible annotations and attributes
     --
-    -- a new region is created upon each attr definition or deletion,
+    -- a new region is created upon each (group of) attr definition or deletion,
     -- consecutive regions within a scope should be ever expanding in src span
     -- regards
     --
@@ -61,59 +144,12 @@ data EL'Region = EL'Region
   { el'region'span :: !SrcRange,
     -- | an annotation is created by the (::) operator, with left-hand operand
     -- being the attribute key
-    el'region'annos :: !(HashMap AttrKey ExprSrc),
+    el'region'annos :: !(HashMap EL'AttrKey ExprSrc),
     -- | an attribute is created by any form of assignment targeting current
     -- scope, or any form of procedure declaration
-    el'region'attrs :: !(HashMap AttrKey EL'Value),
+    el'region'attrs :: !(HashMap EL'AttrKey EL'OriginalValue),
     -- | an effectiful attribute is created by any form of assignment targeting
     -- current scope, or any form of procedure declaration, which follows an
     -- `effect` keyword, or within a block following an `effect` keyword
-    el'region'effs :: !(HashMap AttrKey EL'Value)
+    el'region'effs :: !(HashMap EL'AttrKey EL'OriginalValue)
   }
-
-data EL'Class = EL'Class
-  { el'class'scope :: !EL'Scope,
-    -- | an attribute is exported by any form of assignment targeting
-    -- current scope, or any form of procedure declaration, which follows an
-    -- `export` keyword, or within a block following an `export` keyword
-    el'class'exports :: !EL'Exports
-  }
-
--- | the dict of artifacts by attribute key, for those exported from an object
--- (a module object or vanilla object), with their order of export preserved
-type EL'Exports = OrderedDict AttrKey EL'Value
-
--- | an Edh language level value is created by any form of assignment targeting
--- current scope, or any form of procedure declaration
-data EL'Value = EL'Value
-  { el'value'span :: !SrcRange,
-    el'value'src :: !ExprSrc,
-    el'value'refied :: !(TVar EdhValue)
-  }
-
--- | Edh module and `.edh` text doc (os file, virtual or physical, local or
--- remote) be of 1:1 mapping
-data EL'Module = EL'Module
-  { -- | each parent dir of `edh_modules` is considered an Edh home
-    el'modu'home :: !EL'Home,
-    -- | absolute path of the `.edh` src file
-    el'modu'doc :: !SrcDoc,
-    -- | there will be nested scopes appearing in natural source order, within
-    -- this root scope of the module
-    el'modu'scope :: !EL'Scope,
-    -- | an attribute is exported by any form of assignment targeting
-    -- current scope, or any form of procedure declaration, which follows an
-    -- `export` keyword, or within a block following an `export` keyword
-    el'modu'exports :: !EL'Exports
-  }
-
-data EL'Home = EL'Home
-  { -- | each parent dir of `edh_modules` is considered an Edh home
-    el'home'path :: !Text,
-    -- | loaded modules under this home
-    -- module name is path (with `.edh` and `/__init__.edh` stripped off), and
-    -- relative to the `edh_modules` dir
-    el'home'modules :: !(TMVar (Map.HashMap ModuleName EL'Module))
-  }
-
-type ModuleName = Text
