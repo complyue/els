900c437b48bc349b8a2c93783bf59a860d9c3b9f
ComplYue <compl.yue@icloud.com>
2021-01-07 00:10:36 +0800

    suggest literals as intrinsic completion items

--- host.hs/src/Language/Edh/Meta/Analyze.hs
+++ host.hs/src/Language/Edh/Meta/Analyze.hs
@@ -4042,79 +4042,275 @@ suggestCompletions !elw !line !char !modu =
         suggestAmbientArt (AttrByName !artName, _) =
           not $ "__" `T.isPrefixOf` artName
 
-        !intrinsicSuggestions = []
-    -- these are available from edh-vscode-syntax as json snippets
-    -- TODO implement them with more dynamic intellisense?
-    -- completionSnippet
-    --   "class"
-    --   "class definition"
-    --   "define a new class"
-    --   "010"
-    --   ( "class ${1:Class'Name} {\n"
-    --       <> "  $2\n}\n"
-    --   ),
-    -- completionSnippet
-    --   "data"
-    --   "data class definition"
-    --   "define a new data class"
-    --   "010"
-    --   "data ${1:Class'Name}( ${2:data'Field1} ) {$3}\n",
-    -- completionSnippet
-    --   "void method"
-    --   "void method definition"
-    --   "define a new void method"
-    --   "010"
-    --   ( "method ${1:method'Name}( ${2:mth'Arg1} ) void {\n"
-    --       <> "  $3\n}\n"
-    --   ),
-    -- completionSnippet
-    --   "method"
-    --   "method definition"
-    --   "define a new method"
-    --   "010"
-    --   ( "method ${1:method'Name}( ${2:mth'Arg1} ) {\n"
-    --       <> "  $3\n}\n"
-    --   ),
-    -- completionSnippet
-    --   "generator"
-    --   "generator definition"
-    --   "define a new generator"
-    --   "010"
-    --   ( "generator ${1:method'Name}( ${2:mth'Arg1} ) {\n"
-    --       <> "  $3\n}\n"
-    --   ),
-    -- completionSnippet
-    --   "interpreter"
-    --   "interpreter definition"
-    --   "define a new interpreter"
-    --   "010"
-    --   ( "interpreter ${1:method'Name}( ${2:mth'Arg1} ) {\n"
-    --       <> "  $3\n}\n"
-    --   ),
-    -- completionSnippet
-    --   "producer"
-    --   "producer definition"
-    --   "define a new producer"
-    --   "010"
-    --   ( "producer ${1:method'Name}( ${2:mth'Arg1}, outlet= sink ) {\n"
-    --       <> "  $3\n}\n"
-    --   )
-    -- ,
-    -- completionToken
-    --   "this"
-    --   "this reference"
-    --   "address the contextual instance of current object"
-    --   "100", -- category
-    -- completionToken
-    --   "that"
-    --   "that reference"
-    --   "address the end instance of current object"
-    --   "100", -- category
-    -- completionToken
-    --   "super"
-    --   "super reference"
-    --   "to address super methods from current object"
-    --   "100" -- category
+        !intrinsicSuggestions =
+          [ completionToken
+              "nil"
+              "nil literal"
+              "the dreaded nothingness"
+              "100", -- category
+            completionToken
+              "true"
+              "true literal"
+              "the literal boolean value"
+              "100", -- category
+            completionToken
+              "false"
+              "false literal"
+              "the literal boolean value"
+              "100", -- category
+            completionToken
+              "sink"
+              "sink constructor"
+              ( "the `sink` literal will construct a new event sink"
+                  <> " each time it is evaluated"
+              )
+              "100", -- category
+            completionToken
+              "nan"
+              "nan literal"
+              "not-a-number, the toxic Decimal value"
+              "100", -- category
+            completionToken
+              "inf"
+              "inf literal"
+              "the infinite Decimal value, can be negated as `-inf`"
+              "100", -- category
+            completionToken
+              "None"
+              "None literal"
+              ( "None carries the same semantics as in Python, while it is "
+                  <> "technically `None := nil` in Edh"
+              )
+              "100", -- category
+            completionToken
+              "Nothing"
+              "Nothing literal"
+              ( "Nothing carries the same semantics as in BASIC, while it is "
+                  <> "technically `Nothing := nil` in Edh"
+              )
+              "100", -- category
+            completionToken
+              "NA"
+              "NA literal"
+              ( "not-applicable, the toxic default value, "
+                  <> "it is technically `default nil"
+              )
+              "100", -- category
+            completionToken
+              "EQ"
+              "literal for _equal_"
+              ( "EQ carries the same semantics as in Haskell, "
+                  <> "while being exactly the same value in the host env"
+              )
+              "100", -- category
+            completionToken
+              "LT"
+              "literal for _less-than_"
+              ( "LT carries the same semantics as in Haskell, "
+                  <> "while being exactly the same value in the host env"
+              )
+              "100", -- category
+            completionToken
+              "GT"
+              "literal for _greater-than_"
+              ( "GT carries the same semantics as in Haskell, "
+                  <> "while being exactly the same value in the host env"
+              )
+              "100", -- category
+            completionToken
+              "DecimalType"
+              "a type value literal"
+              "the type of a numeric value"
+              "100", -- category
+            completionToken
+              "BoolType"
+              "a type value literal"
+              "the type of a boolean value"
+              "100", -- category
+            completionToken
+              "StringType"
+              "a type value literal"
+              "the type of a string value"
+              "100", -- category
+            completionToken
+              "BlobType"
+              "a type value literal"
+              "the type of a blob value"
+              "100", -- category
+            completionToken
+              "SymbolType"
+              "a type value literal"
+              "the type of a symbol value"
+              "100", -- category
+            completionToken
+              "ObjectType"
+              "a type value literal"
+              "the type of an object value"
+              "100", -- category
+            completionToken
+              "DictType"
+              "a type value literal"
+              "the type of a dict value"
+              "100", -- category
+            completionToken
+              "ListType"
+              "a type value literal"
+              "the type of a list value"
+              "100", -- category
+            completionToken
+              "PairType"
+              "a type value literal"
+              "the type of a pair value"
+              "100", -- category
+            completionToken
+              "ArgsPackType"
+              "a type value literal"
+              "the type of an apk value"
+              "100", -- category
+            completionToken
+              "IntrinsicType"
+              "a type value literal"
+              "the type of an intrinsic operator procedure value"
+              "100", -- category
+            completionToken
+              "HostClassType"
+              "a type value literal"
+              "the type of a host class value"
+              "100", -- category
+            completionToken
+              "HostMethodType"
+              "a type value literal"
+              "the type of a host method procedure value"
+              "100", -- category
+            completionToken
+              "HostOperType"
+              "a type value literal"
+              "the type of a host operator procedure value"
+              "100", -- category
+            completionToken
+              "HostGenrType"
+              "a type value literal"
+              "the type of a host generator procedure value"
+              "100", -- category
+            completionToken
+              "ClassType"
+              "a type value literal"
+              "the type of a class value"
+              "100", -- category
+            completionToken
+              "MethodType"
+              "a type value literal"
+              "the type of a method procedure value"
+              "100", -- category
+            completionToken
+              "OperatorType"
+              "a type value literal"
+              "the type of an operator procedure value"
+              "100", -- category
+            completionToken
+              "GeneratorType"
+              "a type value literal"
+              "the type of a generator procedure value"
+              "100", -- category
+            completionToken
+              "InterpreterType"
+              "a type value literal"
+              "the type of a interpreter procedure value"
+              "100", -- category
+            completionToken
+              "ProducerType"
+              "a type value literal"
+              "the type of a producer procedure value"
+              "100", -- category
+            completionToken
+              "DescriptorType"
+              "a type value literal"
+              "the type of a property descriptor value"
+              "100", -- category
+            completionToken
+              "BreakType"
+              "a type value literal"
+              ( "the type of a break value, which will be resulted by"
+                  <> " evaluating a `break` statement"
+              )
+              "100", -- category
+            completionToken
+              "ContinueType"
+              "a type value literal"
+              ( "the type of a continue value, which will be resulted by"
+                  <> " evaluating a `continue` statement"
+              )
+              "100", -- category
+            completionToken
+              "FallthroughType"
+              "a type value literal"
+              ( "the type of a fallthrough value, which will be resulted by"
+                  <> " evaluating a `fallthrough` statement"
+              )
+              "100", -- category
+            completionToken
+              "CaseCloseType"
+              "a type value literal"
+              ( "the type of a case-match value, which will be resulted by"
+                  <> " evaluating a matched branch"
+              )
+              "100", -- category
+            completionToken
+              "CaseOtherType"
+              "a type value literal"
+              ( "the type of a case-mismatch value, which will be resulted by"
+                  <> " evaluating a mismatched branch"
+              )
+              "100", -- category
+            completionToken
+              "RethrowType"
+              "a type value literal"
+              ( "the type of a rethrow value,"
+                  <> " such a value can rarely be seen by Edh code"
+              )
+              "100", -- category
+            completionToken
+              "YieldType"
+              "a type value literal"
+              ( "the type of a yield value,"
+                  <> " such a value can rarely be seen by Edh code"
+              )
+              "100", -- category
+            completionToken
+              "ReturnType"
+              "a type value literal"
+              ( "the type of a return value,"
+                  <> " such a value can rarely be seen by Edh code"
+              )
+              "100", -- category
+            completionToken
+              "OrdType"
+              "a type value literal"
+              ( "the type of an Ord value,"
+                  <> " which can only be one of `EQ` `LT` or `GT`"
+              )
+              "100", -- category
+            completionToken
+              "DefaultType"
+              "a type value literal"
+              "the type of a default value"
+              "100", -- category
+            completionToken
+              "SinkType"
+              "a type value literal"
+              "the type of an event sink value"
+              "100", -- category
+            completionToken
+              "ExprType"
+              "a type value literal"
+              "the type of an expression value"
+              "100", -- category
+            completionToken
+              "TypeType"
+              "a type value literal"
+              "the type of a type value"
+              "100" -- category
+          ]
 
     suggestScopeArt :: SrcRange -> (AttrKey, EL'AttrDef) -> CompletionItem
     suggestScopeArt !replace'span (!key, !def) =
