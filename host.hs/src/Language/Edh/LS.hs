f31917433244114d0149a11714b46eb58e3398e1
ComplYue <compl.yue@icloud.com>
2021-06-03 23:24:12 +0800

    upd

--- host.hs/src/Language/Edh/LS.hs
+++ host.hs/src/Language/Edh/LS.hs
@@ -11,46 +11,43 @@ import Language.Edh.EHI
 import Language.Edh.LS.LangServer
 import Language.Edh.LS.RT
 import Language.Edh.Meta.World
+import Language.Edh.Net
 import Prelude
 
 installLanguageServerBatteries :: EdhWorld -> IO ()
 installLanguageServerBatteries !world =
   void $
-    installEdhModule world "els/RT" $ \ !ets !exit ->
+    installEdhModule world "els/RT" $ \ !ets !exit -> runEdhTx ets $
       -- loosely depend on the @net@ runtime from nedh package
-      runEdhTx ets $
-        importEdhModule "net/RT" $ \ !moduNetRT _ets ->
-          lookupEdhObjAttr moduNetRT (AttrByName "Addr") >>= \case
-            (_, EdhObject !addrClass) -> do
-              let !moduScope = contextScope $ edh'context ets
+      withAddrClass $ \ !clsAddr _ets -> do
+        let !moduScope = contextScope $ edh'context ets
 
-              !msClass <- createMetaModuleClass moduScope
-              !mwClass <- createMetaWorldClass msClass moduScope
-              !lsClass <- createLangServerClass addrClass moduScope
+        !msClass <- createMetaModuleClass moduScope
+        !mwClass <- createMetaWorldClass msClass moduScope
+        !lsClass <- createLangServerClass clsAddr moduScope
 
-              !moduMths <-
-                sequence $
-                  [ (AttrByName nm,) <$> mkHostProc moduScope mc nm hp
-                    | (nm, mc, hp) <-
-                        [ ( "sendTextToFd",
-                            EdhMethod,
-                            wrapHostProc sendTextToFd
-                          )
-                        ]
+        !moduMths <-
+          sequence $
+            [ (AttrByName nm,) <$> mkHostProc moduScope mc nm hp
+              | (nm, mc, hp) <-
+                  [ ( "sendTextToFd",
+                      EdhMethod,
+                      wrapHostProc sendTextToFd
+                    )
                   ]
+            ]
 
-              let !moduArts =
-                    moduMths
-                      ++ [ (AttrByName "MetaModule", EdhObject msClass),
-                           (AttrByName "MetaWorld", EdhObject mwClass),
-                           (AttrByName "LangServer", EdhObject lsClass)
-                         ]
-              !artsDict <-
-                EdhDict
-                  <$> createEdhDict [(attrKeyValue k, v) | (k, v) <- moduArts]
-              flip iopdUpdate (edh'scope'entity moduScope) $
-                [(k, v) | (k, v) <- moduArts]
-                  ++ [(AttrByName "__exports__", artsDict)]
+        let !moduArts =
+              moduMths
+                ++ [ (AttrByName "MetaModule", EdhObject msClass),
+                     (AttrByName "MetaWorld", EdhObject mwClass),
+                     (AttrByName "LangServer", EdhObject lsClass)
+                   ]
+        !artsDict <-
+          EdhDict
+            <$> createEdhDict [(attrKeyValue k, v) | (k, v) <- moduArts]
+        flip iopdUpdate (edh'scope'entity moduScope) $
+          [(k, v) | (k, v) <- moduArts]
+            ++ [(AttrByName "__exports__", artsDict)]
 
-              exit
-            _ -> error "bug: net/RT provides no Addr class"
+        exit
