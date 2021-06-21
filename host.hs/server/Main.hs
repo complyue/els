bab799cf8bb5c5bcc2f8c15fadfc998fc1fd785a
ComplYue <compl.yue@icloud.com>
2021-06-21 22:13:03 +0800

    runmodu upd

--- host.hs/server/Main.hs
+++ host.hs/server/Main.hs
@@ -2,54 +2,19 @@ module Main where
 
 -- import           Debug.Trace
 
-import Control.Concurrent
-import Control.Exception
-import Control.Monad
-import qualified Data.Text as T
 import Language.Edh.EHI
 import Language.Edh.LS
 import Language.Edh.Net
+import Language.Edh.Run
 import Prelude
 
 main :: IO ()
-main = do
-  !console <- defaultEdhConsole defaultEdhConsoleSettings
-  let !consoleOut = consoleIO console . ConsoleOut
-      runIt = do
-        world <- createEdhWorld console
-        installEdhBatteries world
+main = edhRunModule defaultEdhConsoleSettings "els" $
+  \ !world -> do
+    -- install all necessary batteries
+    installEdhBatteries world
+    installNetBatteries world
+    installLanguageServerBatteries world
 
-        -- install batteries provided by nedh
-        installNetBatteries world
-
-        -- install batteries provided by els
-        installLanguageServerBatteries world
-
-        runEdhModule world "els" edhModuleAsIs >>= \case
-          Left !err -> do
-            -- program crash on error
-            consoleOut "Đ (Edh) language server crashed with an error:\n"
-            consoleOut $ T.pack $ show err <> "\n"
-          Right !phv -> case edhUltimate phv of
-            -- clean program halt, all done
-            EdhNil -> return ()
-            -- unclean program exit
-            _ -> do
-              consoleOut "Đ (Edh) language server halted with a result:\n"
-              consoleOut $
-                (<> "\n") $ case phv of
-                  EdhString msg -> msg
-                  _ -> T.pack $ show phv
-
-  void $
-    forkFinally runIt $ \ !result -> do
-      case result of
-        Left (e :: SomeException) ->
-          consoleOut $ "💥 " <> T.pack (show e)
-        Right _ -> pure ()
-      -- shutdown console IO anyway
-      consoleIO console ConsoleShutdown
-
-  consoleOut ">> Đ (Edh) Language Server <<\n"
-
-  consoleIOLoop console
+    let !consoleOut = consoleIO (edh'world'console world) . ConsoleOut
+    consoleOut ">> Đ (Edh) Language Server <<\n"
